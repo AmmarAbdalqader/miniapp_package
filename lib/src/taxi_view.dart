bcd394707f33ec84b2063bde4fea9e9bbb75eee1
ammarabdalqader <ammar.khalid513@gmail.com>
2025-10-30 16:27:49 +0300

    return data

--- lib/src/taxi_view.dart
+++ lib/src/taxi_view.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 class TaxiView extends StatelessWidget {
-  const TaxiView({super.key});
+  final Function(String data) onSuccess;
+  const TaxiView({super.key, required this.onSuccess});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-        itemCount: 100,
-        separatorBuilder: (context, index) => const SizedBox(height: 10),
-        itemBuilder: (context, index) => ListTile(
-          title: Text("Title"),
-          subtitle: Text('sub title sub title sub title sub title sub title '),
-          leading: Icon(Icons.car_crash),
+      body: InkWell(
+        onTap: () => onSuccess("ammar"),
+        child: ListView.separated(
+          itemCount: 100,
+          separatorBuilder: (context, index) => const SizedBox(height: 10),
+          itemBuilder: (context, index) => ListTile(
+            title: Text("Title"),
+            subtitle:
+                Text('sub title sub title sub title sub title sub title '),
+            leading: Icon(Icons.car_crash),
+          ),
         ),
       ),
     );

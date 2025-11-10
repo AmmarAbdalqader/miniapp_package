304d641805935365b1c2fcc0b456a74e7b7d6dfa
ammarabdalqader <ammar.khalid513@gmail.com>
2025-11-10 11:19:30 +0300

    edit design

--- lib/src/taxi_view.dart
+++ lib/src/taxi_view.dart
@@ -6,41 +6,70 @@ class TaxiView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.white,
-        body: ListView.separated(
-          itemCount: 100,
-          separatorBuilder: (context, index) => const Divider(),
-          itemBuilder: (context, index) => Row(
+    backgroundColor: Colors.white,
+    body: ListView.separated(
+      itemCount: 100,
+      separatorBuilder: (context, index) => const Divider(),
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      itemBuilder: (context, index) => Row(
+        children: [
+          Column(
             children: [
-              Column(
-                children: [
-                  Image.asset(
-                    height: 30,
-                    width: 30,
-                    'assets/logos/${index % 2 == 0 ? 'careem' : 'uber'}.png',
-                    package: 'miniapp_package',
-                  ),
-                  const SizedBox(height: 12),
-                  Image.asset(
-                    height: 40,
-                    width: 40,
-                    'assets/logos/${index % 2 == 1 ? 'sedan' : 'suv'}.png',
-                    package: 'miniapp_package',
-                  ),
-                ],
+              Image.asset(
+                height: 40,
+                width: 40,
+                fit: BoxFit.contain,
+                'assets/logos/${index % 2 == 0 ? 'careem' : 'uber'}.png',
+                package: 'miniapp_package',
               ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text("Title"),
-                    Text('sub title sub title sub title sub title sub title '),
-                  ],
-                ),
+              const SizedBox(height: 12),
+              Image.asset(
+                height: 80,
+                width: 80,
+                'assets/logos/${index % 2 == 1 ? 'sedan' : 'suv'}.png',
+                package: 'miniapp_package',
               ),
-              ElevatedButton(
-                  onPressed: () => onSuccess("ammar"), child: Text("Let's Go"))
             ],
           ),
-        ),
-      );
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Trip type"),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    Icon(Icons.access_time_outlined),
+                    const SizedBox(width: 8),
+                    Text("40 Mins . 3.6 KM", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                RichText(
+                  text: TextSpan(
+                    style: const TextStyle(
+                        fontSize: 13, color: Colors.black87, height: 1.3),
+                    children: [
+                      const TextSpan(text: 'approx '),
+                      TextSpan(
+                        text: '15 SR . ',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const TextSpan(text: '5 mins away'),
+                    ],
+                  ),
+                  textAlign: TextAlign.right,
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () => onSuccess("ammar"), child: Text("Choose trip"))
+        ],
+      ),
+    ),
+  );
 }
